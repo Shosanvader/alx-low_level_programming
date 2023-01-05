293fbaaa794845eaa598229753119e8b671f619c
Shosan Vader <okediadip@gmail.com>
2023-01-05 13:13:38 +0100

    Catching up on undone projects

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,6 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
-
-#endif
+void print_number(int n);
